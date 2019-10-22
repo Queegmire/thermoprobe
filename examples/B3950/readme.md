034d5e0e5df90ed8c57321821b59657c7fee6a3f
Ryan Johnson <rjohnson@queegmire.com>
2019-10-22 01:45:50 -0700

    Rough calibration of  thermistor
    Link to  Steinhart–Hart calculator in readme

--- examples/B3950/readme.md
+++ examples/B3950/readme.md
@@ -1,15 +1,11 @@
-
-
 DROK Temperature Sensor Probe, 10k Ohm - [Source](https://www.amazon.com/DROK-Waterproof-Temperature-Thermistor-Transimitter/dp/B01MR37GOQ/)
 
 - Type : NTC 10K/B3950 1%
 
+# Steinhart–Hart equation
+- [Wikipedia](https://en.wikipedia.org/wiki/Steinhart–Hart_equation)
+- [calculator](https://rusefi.com/Steinhart-Hart.html)
+
+Code based on [Thermistor Interfacing with NodeMCU](https://www.electronicwings.com/nodemcu/thermistor-interfacing-with-nodemcu)
+
 Note: ADC2 pins and wifi can have conflicts on ESP32 Devkit v1 DOIT. Use ADC1 pin.
-
-Use [Steinhart–Hart equation](https://en.wikipedia.org/wiki/Steinhart–Hart_equation)
-
-Readings with current settings seem good at room temperature, but are too low as the temperature rises.
-How to recalculate?
-
-Code base on [Thermistor Interfacing with NodeMCU](https://www.electronicwings.com/nodemcu/thermistor-interfacing-with-nodemcu)
-
--- examples/B3950/src/main.cpp
+++ examples/B3950/src/main.cpp
@@ -4,9 +4,15 @@ const double VCC = 3.3;
 const double R = 10000; // 10k ohm
 const double adc_resolution = 4095; // 12-bit
 
+/*
 const double A = 0.001129148;
 const double B = 0.000234125;
 const double C = 0.0000000876741; 
+*/
+
+const double A = 0.0010134767412286589;
+const double B = 0.00023948035951795824;
+const double C = 0.00000008915888326629528; 
 
 void setup() {
   Serial.begin(115200);
@@ -16,8 +22,14 @@ void loop() {
   double Vout, Rth, temperature, adc_value; 
 
   adc_value = analogRead(GPIO_NUM_34);
+  Serial.print("adc_value = ");
+  Serial.println(adc_value);
   Vout = (adc_value * VCC) / adc_resolution;
+  Serial.print("Vout      = ");
+  Serial.println(Vout);
   Rth = (VCC * R / Vout) - R;
+  Serial.print("Rth       = ");
+  Serial.println(Rth);
 
 /*  Steinhart-Hart Thermistor Equation:
  *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
@@ -28,5 +40,5 @@ void loop() {
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" degree celsius");
-  delay(2000);
+  delay(2500);
 }
